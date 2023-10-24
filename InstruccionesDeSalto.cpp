72c3e6f4f33e0692cfa5faaa25955454dd907bc5
jsepucas <jsepucas@myuax.com>
2023-10-24 19:22:14 +0200

    Instruccionesdesalto

--- InstruccionesDeSalto.cpp
+++ InstruccionesDeSalto.cpp
@@ -1,1 +1,17 @@
-/
+#include <iostream>
+int main() {
+    const int limite = 100;
+    int suma = 0;
+    int numero;
+    while (true) {
+        std::cout << "Registre un numero: ";
+        std::cin >> numero;
+        if (suma + numero > limite) {
+            std::cout << "La suma total ha superado el limite de " << limite << ". Saliendo del bucle." <<std::endl;
+        }
+        suma += numero;
+    }
+    std::cout << "La suma total es: " << suma << std::endl;
+
+    return 0;
+}
